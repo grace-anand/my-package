e2bcd7e3dad41ba3cbe890f25e21544a14eda56e
anandr-ms <anand.r@moneysmart.com>
2022-08-31 12:00:43 +0530

    Initialize package with base setup 🥳

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+const Footer = (): JSX.Element => {
+  return (
+    <footer className={styles.footer}>
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
